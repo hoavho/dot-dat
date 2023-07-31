e3176c8784ba512083deee1941a038a4045dfe7f
Hoa Ho <hoviethoa@gmail.com>
2023-07-31 10:29:14 +0700

    Update pyenv-config

--- zsh/.config/zsh/pyenv-config.zsh
+++ zsh/.config/zsh/pyenv-config.zsh
@@ -1,8 +1,6 @@
 if [ -n "${PYENV_ENABLED:+1}" ]; then
-  HISTFILE=
   export PYENV_ROOT="${XDG_DATA_HOME:-$HOME/.local/share}/pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  export PATH="$PYENV_ROOT/shims:${PATH}"
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 fi
 
