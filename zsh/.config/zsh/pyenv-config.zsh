1acc1f6616d0d46da5af1da7efccd00fe53e146f
Hoa Ho <hoa.ho@btcx.com>
2023-08-02 13:50:34 +0700

    Update pyenv-config.zsh

--- zsh/.config/zsh/pyenv-config.zsh
+++ zsh/.config/zsh/pyenv-config.zsh
@@ -1,6 +1,6 @@
 if [ -n "${PYENV_ENABLED:+1}" ]; then
   export PYENV_ROOT="${XDG_DATA_HOME:-$HOME/.local/share}/pyenv"
-  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
+  command -v pyenv > /dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  command -v pyenv > /dev/null && eval "$(pyenv init -)"
 fi
 
