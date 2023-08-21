a8fab6e2ae4030849e01a00bce32f9c77f2c147a
Hoa Ho <hoviethoa@gmail.com>
2023-08-20 22:51:57 -0700

    Add comment to pyenv-virtualenv-config

--- zsh/.config/zsh/pyenv-virtualenv-config.zsh
+++ zsh/.config/zsh/pyenv-virtualenv-config.zsh
@@ -1,3 +1,4 @@
 if [ -n "${PYENV_VIRTUAL_ENV_ENABLED:+1}" ]; then
+  # enable auto-activation of virtualenvs
   if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 fi
