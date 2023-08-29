daeccc6acc75103ab7e600605c30703cd15d4abf
Hoa Ho <hoviethoa@gmail.com>
2023-08-28 21:50:54 -0700

    lazy load pyenv-virtualenv

--- zsh/.config/zsh/pyenv-virtualenv-config.zsh
+++ zsh/.config/zsh/pyenv-virtualenv-config.zsh
@@ -1,4 +1,20 @@
 if [ -n "${PYENV_VIRTUAL_ENV_ENABLED:+1}" ]; then
-  # enable auto-activation of virtualenvs
-  if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+  # Try to find pyenv, if it's not on the path
+  export PYENV_ROOT="${XDG_DATA_HOME:-$HOME/.local/share}/pyenv"
+  if ! type pyenv > /dev/null && [ -f "${PYENV_ROOT}/bin/pyenv" ]; then
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+  fi
+
+  # Lazy load pyenv
+  if type pyenv > /dev/null; then
+    export PATH="${PYENV_ROOT}/bin:${PYENV_ROOT}/shims:${PATH}"
+    function pyenv() {
+        unset -f pyenv
+        eval "$(command pyenv init -)"
+        if [[ -n "${ZSH_PYENV_LAZY_VIRTUALENV}" ]]; then
+            eval "$(command pyenv virtualenv-init -)"
+        fi
+        pyenv $@
+    }
+  fi
 fi
