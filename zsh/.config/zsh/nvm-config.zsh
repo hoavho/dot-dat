2ca4a2d3576c971a5a7bd01f97ac7d381e55cc41
Hoa Ho <hoviethoa@gmail.com>
2024-05-01 23:56:48 -0700

    Update nvm-config - handle intel chip vs Apple silicon chip

--- zsh/.config/zsh/nvm-config.zsh
+++ zsh/.config/zsh/nvm-config.zsh
@@ -1,5 +1,15 @@
 if [ -n "${NVM_ENABLED:+1}" ]; then
   export NVM_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && source /opt/homebrew/opt/nvm/nvm.sh # This load nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm # This load nvm bash_completion -> works fine with zsh
+  case `uname -p` in
+    i386)
+      # commands for Mac intel chip
+      [ -s "/usr/local/opt/nvm/nvm.sh" ] && source /usr/local/opt/nvm/nvm.sh # This load nvm
+      [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source /usr/local/opt/nvm/etc/bash_completion.d/nvm # This load nvm bash_completion -> works fine with zsh
+    ;;
+    arm)
+      # commands for Mac Apple silicon chip
+      [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && source /opt/homebrew/opt/nvm/nvm.sh # This load nvm
+      [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm # This load nvm bash_completion -> works fine with zsh
+    ;;
+  esac
 fi
