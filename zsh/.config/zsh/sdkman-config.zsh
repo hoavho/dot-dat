5d879c8c42354904b19b403a338c585c10f3aa12
Hoa Ho <hoviethoa@gmail.com>
2024-05-02 00:02:13 -0700

    Add comments to sdkman-config.zsh

--- zsh/.config/zsh/sdkman-config.zsh
+++ zsh/.config/zsh/sdkman-config.zsh
@@ -1,3 +1,6 @@
+# If you already installed sdkman (to default location at ~/.sdkman)
+# -> you need to move it to $HOME/.local/share/sdkman 
+# -> so run "mv ~/.sdkman ~/.local/share/sdkman"
 if [ -n "${SDKMAN_ENABLED:+1}" ]; then
   export SDKMAN_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/sdkman"
   [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
