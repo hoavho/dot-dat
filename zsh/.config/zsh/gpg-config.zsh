e368ed28475c2f84f1dbe2e609d4ebb0f8f72460
Hoa Ho <hoviethoa@gmail.com>
2024-06-13 15:05:01 -0700

    clean up

--- zsh/.config/zsh/gpg-config.zsh
+++ zsh/.config/zsh/gpg-config.zsh
@@ -1,7 +1,6 @@
 # https://www.gnupg.org/documentation/manuals/gnupg/GPG-Configuration.html
 #
 if [ -n "${GPG_ENABLED:+1}" ]; then
-  echo "here"
   export GNUPGHOME="${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"
   if [ ! -d "$GNUPGHOME" ]; then
     echo "$GNUPGHOME not found. Creating it ..."
